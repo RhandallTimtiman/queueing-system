793320c6be7900162d03cd618158f1d4385a0ddb
Rhandall Timtiman <rhandall.timtiman@myxlog.com>
2020-08-06 11:47:38 +0800

    Initial Commit

---
+++ src/pages/Patient/PatientLanding.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page-container>
+    <q-page>
+      <div class="fit row">
+        <img class="background" src="~assets/6460.jpg" alt="" />
+      </div>
+    </q-page>
+  </q-page-container>
+</template>\
+<style lang="sass" scoped>
+.background
+  width: 100%
+  height: 100%
+</style>
