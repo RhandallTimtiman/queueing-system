064cfdf5cbe4a4c94c1df9e489557ff43e0192e5
Rhandall Timtiman <rhandall.timtiman@myxlog.com>
2020-09-09 20:53:34 +0800

    Added BG File

--- src/layouts/Patient/PatientLayout.vue
+++ src/layouts/Patient/PatientLayout.vue
@@ -1,60 +1,5 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-drawer
-      show-if-above
-      v-model="right"
-      side="right"
-      elevated
-      content-class="bg-teal text-white"
-      :width="300"
-      :breakpoint="700"
-    >
-      <div class="fit column justify-center items-center content-center">
-        <div class="q-pa-md full-width">
-          <div v-if="step === 1">
-            <div class="text-h4 text-center q-mb-md">
-              Do you have an appointment?
-            </div>
-            <q-btn
-              color="primary"
-              class="full-width"
-              icon-right="send"
-              label="Set an appointment"
-              @click="next()"
-            />
-            <div class="text-h6 text-center q-mb-md">
-              Or
-            </div>
-            <q-btn
-              color="primary"
-              class="full-width"
-              icon-right="send"
-              label="Existing Patient"
-              @click="next()"
-            />
-          </div>
-          <div class="full-width" v-if="step === 2">
-            <q-input
-              color="white"
-              filled
-              v-model="information.firstName"
-              label="First Name"
-              stack-label
-              :dense="true"
-            />
-            <q-input
-              color="white"
-              filled
-              v-model="information.lastName"
-              label="Last Name"
-              stack-label
-              :dense="true"
-            />
-          </div>
-        </div>
-      </div>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -64,19 +9,9 @@
 <script>
 export default {
   data() {
-    return {
-      right: false,
-      step: 1,
-      information: {
-        firstName: "",
-        lastName: ""
-      }
-    };
-  },
-  methods: {
-    next() {
-      this.step += 1;
-    }
+    return {};
   }
 };
 </script>
+<style lang="sass">
+</style>
--- src/pages/Patient/PatientLanding.vue
+++ src/pages/Patient/PatientLanding.vue
@@ -2,13 +2,29 @@
   <q-page-container>
     <q-page>
       <div class="fit row">
-        <img class="background" src="~assets/6460.jpg" alt="" />
+        <img class="background" src="~assets/doctor-bg.svg" alt="" />
+        <div class="headers">
+          <h1 class="text-weight-bold">WOULD YOU LIKE TO SET</h1>
+          <h1 class="headers-left text-weight-bold">AN APPOINTMENT</h1>
+          <q-btn elevated size="xl" rounded color="primary" label="Unelevated Rounded" />
+        </div>
       </div>
     </q-page>
   </q-page-container>
-</template>\
+</template>
 <style lang="sass" scoped>
 .background
-  width: 100%
-  height: 100%
+  height: 760px
+  position: absolute;
+  top: 24vh;
+  left: 15vh;
+
+.headers
+  position: absolute;
+  top: 24vh;
+  left: 65vh;
+
+.headers-left
+  margin-left: 271px;
+
 </style>
