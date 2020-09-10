6ce9a9afca91a40d5ff7939427b9fc08e0b02df2
Rhandall Timtiman <rhandall.timtiman@myxlog.com>
2020-09-10 18:49:41 +0800

    Initialized Col

--- src/pages/Patient/PatientLanding.vue
+++ src/pages/Patient/PatientLanding.vue
@@ -1,23 +1,18 @@
 <template>
   <q-page-container>
-    <q-page>
-      <div class="fit row">
+    <q-page class="row q-pa-md">
+      <div class="col self-end offset-md-1">
         <img class="background" src="~assets/doctor-bg.svg" alt="" />
-        <div class="headers">
-          <h1 class="text-weight-bold">WOULD YOU LIKE TO SET</h1>
-          <h1 class="headers-left text-weight-bold">AN APPOINTMENT</h1>
-          <q-btn elevated size="xl" rounded color="primary" label="Unelevated Rounded" />
-        </div>
+      </div>
+      <div class="col self-center">
+        <h1>TEst</h1>
       </div>
     </q-page>
   </q-page-container>
 </template>
 <style lang="sass" scoped>
 .background
-  height: 760px
-  position: absolute;
-  top: 24vh;
-  left: 15vh;
+  height: 680px
 
 .headers
   position: absolute;
