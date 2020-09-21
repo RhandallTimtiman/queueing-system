ee5f9f3992c42c6d7fb0b47e73551446cc3fed61
Rhandall Timtiman <rhandall.timtiman@myxlog.com>
2020-09-21 18:07:01 +0800

    Doctor Greeting

--- src/pages/Doctor/Dashboard.vue
+++ src/pages/Doctor/Dashboard.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="">
-
+  <div class="row">
+    <div class="col-6">
+      <div class="greet-header">
+        <p class="greet-name">Hello Dr. Strange. Welcome Back!</p>
+        <p class="greet-day">Have a wonderful day!</p>
+      </div>
+    </div>
+    <div class="col-6">
+    </div>
   </div>
 </template>
 <style lang="sass" scoped>
+.greeting
+  font-size: x-large;
+
+.greet-header
+  margin-top: 60px;
+
+.greet-day
+  font-size: 45px;
+  color: #6D62FE;
+
+.greet-name
+  font-size: 20px
+  margin-bottom: 0px
 </style>
